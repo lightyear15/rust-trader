8cf2f5fcf8672e6f6407ec33a777523b2742ee27
lightyear15 <g.minist8@gmail.com>
2021-06-19 17:42:40 +0200

    compiling ta_lib

--- build.rs
+++ build.rs
@@ -1,11 +1,21 @@
-
 fn main() {
-    //println!("cargo:rerun-if-changed=./ta_lib");
+    println!("cargo:rerun-if-changed=./ta_lib");
     let commons = std::fs::read_dir("./ta_lib/src/ta_common/").expect("error reading ta_common");
-    //let srcs = std::fs::read_dir("./ta_lib/src/ta_func").expect("error in reading ta_func");
+    let srcs = std::fs::read_dir("./ta_lib/src/ta_func").expect("error in reading ta_func");
+    let src_files_iter = commons.chain(srcs).filter_map(|res_dir| {
+        let dir_entry = res_dir.expect("error reading src_files");
+        if dir_entry.file_type().expect("in reading file type").is_dir() {
+            return None;
+        }
+        let path = dir_entry.path();
+        let extension = path.extension().expect("missing extension");
+        if extension != "c" {
+            return None;
+        }
+        Some(path)
+    });
     cc::Build::new()
-        .files(commons.map(|res_dir| res_dir.expect("error reading path in common").path()))
-        //.files(srcs.map(|res_dir| res_dir.expect("error reading path in srcs").path()))
+        .files(src_files_iter)
         .include("./ta_lib/include")
         .include("./ta_lib/src/ta_common")
         .compile("ta_lib")
